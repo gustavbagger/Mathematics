2e22103bc63bbb9ba9bccf7782dc4538c2e24864
gustavbagger <gustavbagger@me.com>
2025-11-07 16:34:18 +1100

    testing build.sh

--- src/main.py
+++ src/main.py
@@ -3,10 +3,6 @@ from generate_page import generate_page, generate_pages_recursive
 import sys
 
 def main():
-    if len(sys.argv)>1:
-        basepath = sys.argv[1]
-    else:
-        basepath = "/"
     copy_and_override("static","docs")
-    generate_pages_recursive("content/","template.html","docs/",basepath=basepath)
+    generate_pages_recursive("content/","template.html","docs/",basepath=sys.argv[1])
 main()
